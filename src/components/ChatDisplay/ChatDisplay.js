35b66b88140f7848897d5e16d2d4cb564ffa692c
HumphreyJ <joshhumphrey1@gmail.com>
2019-10-27 23:12:58 -0500

    Added some dummy data to fill out the app.

--- src/components/ChatDisplay/ChatDisplay.js
+++ src/components/ChatDisplay/ChatDisplay.js
@@ -4,10 +4,28 @@ import './ChatDisplay.css'
 const ChatDisplay = () => {
     return (
         <div className='chat-display'>
+
            <div className="chat-entry">
                <h4 className="user-name">StormDwarf:</h4>
                <p className="chat-message">Is there an Admin on?</p>
            </div>
+           <div className="chat-entry">
+               <h4 className="user-name">GrassyGnoll:</h4>
+               <p className="chat-message">Not right now.</p>
+           </div>
+           <div className="chat-entry">
+               <h4 className="user-name">BarPatron:</h4>
+               <p className="chat-message">This is some sweey dummy data.</p>
+           </div>
+           <div className="chat-entry">
+               <h4 className="user-name">StormDwarf:</h4>
+               <p className="chat-message">lol yeah.</p>
+           </div>
+           <div className="chat-entry">
+               <h4 className="user-name">CleverUserName:</h4>
+               <p className="chat-message">Wanna Duel?</p>
+           </div>
+
         </div>
     );
 }
